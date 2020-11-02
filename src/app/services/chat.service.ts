09b29d2f86cd27fd9373bd81b219b52998ff224e
Juan <juanalexandro.roblesaraiza@gmail.com>
2020-11-01 23:21:43 -0600

    chat
    service

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -10,44 +10,40 @@ import {environment} from 'src/environments/environment'
 export class ChatService {
 
 // url = "https://proyectotapatio.com/PT-API-P/repartidores/";
-url = environment.apiUrl;
+url = environment.apiUrl+"casas/chat/";
 
 constructor(private http:HttpClient) { }
 
-bloquearChat( id_chat:number, mensaje:string){
-  return this.http.get(`${this.url}chats/bloquearChat.php?id_chat=${id_chat}&mensaje=${mensaje}`)//.pipe(retry(3))
-}
-
 cambiarEstadoNotificacion( id_chat:number){
-  return this.http.get(`${this.url}chats/cambiarEstadoNotificacion.php?id_chat=${id_chat}`)//.pipe(retry(3))
+  return this.http.get(`${this.url}cambiarEstadoNotificacion.php?id_chat=${id_chat}`)//.pipe(retry(3))
 }
 
 cancelarChat( id_chat:number, mensaje:string){
-  return this.http.get(`${this.url}chats/cancelarChat.php?id_chat=${id_chat}&mensaje=${mensaje}`)//.pipe(retry(3))
-}
-
-confirmarCompra(id_chat:any){
-  return this.http.get(`${this.url}chats/confirmarCompra.php?id_chat=${id_chat}`)//.pipe(retry(3))
+  return this.http.get(`${this.url}cancelarChat.php?id_chat=${id_chat}&mensaje=${mensaje}`)//.pipe(retry(3))
 }
 
 enviarMensaje(id_chat:number, mensaje:string){
-  return this.http.get(`${this.url}chats/enviarMensaje.php?id_chat=${id_chat}&mensaje=${mensaje}`)//.pipe(retry(3))
+  return this.http.get(`${this.url}enviarMensaje.php?id_chat=${id_chat}&mensaje=${mensaje}`)//.pipe(retry(3))
 }
 
-filtroChat(estado:number){
-  return this.http.get(`${this.url}chats/filtroChat.php?estado=${estado}`)//.pipe(retry(3))
-}
-
-verChatsNotificacion(){
-  return this.http.get(`${this.url}chats/verChatsNotificacion.php`)//.pipe(retry(3))
+verChats(){
+  return this.http.get(`${this.url}verChats.php`)//.pipe(retry(3))
 }
 
 verDatosChat(id_chat:number){
-  return this.http.get(`${this.url}chats/verDatosChat.php?id_chat=${id_chat}`)//.pipe(retry(3))
+  return this.http.get(`${this.url}verDatosChat.php?id_chat=${id_chat}`)//.pipe(retry(3))
 }
 
 verMensajesChat(id_chat:number){
-  return this.http.get(`${this.url}chats/verMensajesChat.php?id_chat=${id_chat}`)//.pipe(retry(3))
+  return this.http.get(`${this.url}verMensajesChat.php?id_chat=${id_chat}`)//.pipe(retry(3))
+}
+
+crearChat(id_usuario:number, id_oferta:number, mensaje:string){
+  return this.http.get(`${this.url}crearChat.php?id_usuario=${id_usuario}&id_oferta=${id_oferta}&mensaje=${mensaje}`)//.pipe(retry(3))
+}
+
+comprobarChat(id_usuario:number){
+  return this.http.get(`${this.url}comprobarChat.php?id_usuario=${id_usuario}`)//.pipe(retry(3))
 }
 
 
